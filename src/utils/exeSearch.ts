2cadd9de41c7e8c9d8a189ff3b6089832a570c21
NachomPls <NachomPls@users.noreply.github.com>
2021-01-12 01:00:32 +0100

    add class ExeSearcher

--- src/utils/exeSearch.ts
+++ src/utils/exeSearch.ts
@@ -4,29 +4,37 @@ import { fstat } from "fs";
 import fs from "fs";
 import { Queue } from 'queue-typescript';
 
-export function creatGameOverviews(section: Section, path: string):GameOverview[] {
-    const gameOverviews: GameOverview[] = [];
 
-    //Get paths of game folders
-    fs.readdir(path, { withFileTypes: true }, (err, files) => {
-        console.log({err, files});
-        files.forEach(file => {
-            const gameOverview: GameOverview;
-            const queue = new Queue<string>();
+export class ExeSearcher {
 
-            //queue.dequeue()
-            //queue.enqueue()
-            //Search folder for exe, on sucess next folder
-            //Recursion!
+    private queue: Queue<string>;
 
-            //Look at all files, find exe, find folders
+    public ExeSearcher() {
+        this.queue = new Queue<string>();
+    }
 
-
-
-            gameOverviews.push(gameOverview);
+    creatGameOverviews(section: Section, path: string):GameOverview[] {
+        const gameOverviews: GameOverview[] = [];
+    
+        //Get paths of game folders
+        fs.readdir(path, { withFileTypes: true }, (err, files) => {
+            console.log({err, files});
+            files.forEach(file => {
+    
+                //queue.dequeue()
+                //queue.enqueue()
+                //Search folder for exe, on sucess next folder
+                //Recursion!
+    
+                //Look at all files, find exe, find folders
+    
+    
+    
+                gameOverviews.push(gameOverview);
+            })
         })
-    })
-    return gameOverviews;
-}
+        return gameOverviews;
+    }
 
-function exeSearch():boolean|string
+    exeSearch():boolean|string
+}
